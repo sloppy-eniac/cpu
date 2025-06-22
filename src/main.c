1df502518eaf00d88f6f8690be54174923b68149
orangejack <79419803+kingcat47@users.noreply.github.com>
2025-06-22 21:08:51 +0900

    cpu랑 main함수에 뭐해야할지 주석 달아놓음.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,12 @@
 #include <string.h>
+#include "interpreter.c"
+#include "include/memory.h"
+#include "cpu.c"
 
 void main()
 {
-    memcpy(메모리, 버퍼, sizeof(메모리)); // 버퍼 → 메모리 로 복사
+    init_memory(&memory);                        // 메모리 초기화화
+    memcpy(Memory, buffer, sizeof(MEMORY_SIZE)); // 버퍼 → 메모리 로 복사
+    // 레지스터의 pc가 가르키는 주소를 메모리 처음 주소로 설정.
+    cpu(); // cpu.c에 만들어놓은 함수 실행행
 }
